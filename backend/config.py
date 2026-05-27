7e54d595dfc49bcb154876a3f211c49be11c7ed1
Biased-PJ <jain7parth@gmail.com>
2026-05-27 14:16:14 +0530

    Trying to deploy on Render

--- backend/config.py
+++ backend/config.py
@@ -1,13 +1,10 @@
 import os
 from pymongo import MongoClient
-from dotenv import load_dotenv
-
-load_dotenv()
 
 MONGO_URI = os.getenv("MONGO_URI")
 
 if not MONGO_URI:
-    raise Exception("MONGO_URI is missing in environment variables")
+    raise Exception("MONGO_URI not found in Render environment variables")
 
 client = MongoClient(MONGO_URI, serverSelectionTimeoutMS=5000)
 

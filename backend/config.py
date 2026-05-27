92b08f5e9f80db7f2c08e674745f6f4b307cf633
Biased-PJ <jain7parth@gmail.com>
2026-05-27 12:45:34 +0530

    Trying to Deploy

--- backend/config.py
+++ backend/config.py
@@ -5,11 +5,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 MONGO_URI = os.getenv("MONGO_URI")
-SECRET_KEY = os.getenv("SECRET_KEY")
-JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
 
 if not MONGO_URI:
     raise Exception("MONGO_URI not found")
 
-client = MongoClient(MONGO_URI)
+try:
+    client = MongoClient(MONGO_URI, serverSelectionTimeoutMS=5000)
+    client.server_info()
+except Exception as e:
+    print("MongoDB connection error:", e)
+
 db = client["preppilot_ai"]
